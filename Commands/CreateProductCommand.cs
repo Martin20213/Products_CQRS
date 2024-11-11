5bbd5dd81ba99c26bdbf783bc544297854768282
Martin20213 <monster20213@gmail.com>
2024-11-11 14:55:34 +0100

    Command (Hozzáadás)

---
+++ Commands/CreateProductCommand.cs
@@ -0,0 +1,35 @@
+﻿
+using MediatR;
+using Products_CQRS.Data;
+using Products_CQRS.Domain.Models;
+namespace Products_CQRS.Commands
+
+{
+    public record CreateProductCommand(string Name, int Price, int CategoryId) : IRequest<int>;
+    
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, int>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateProductCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name = request.Name,
+                Price = request.Price,
+                CategoryId = request.CategoryId
+            };
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync(cancellationToken);
+            return product.Id;
+        }
+    }
+
+    
+}
